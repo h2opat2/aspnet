6fd3f6cedf4e817a84f7ba5d0cf7117d4723d3db
h2opat2@gmail.com <h2opat2@gmail.com>
2023-04-03 16:56:48 +0200

    Person Detail

--- InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/PersonController.cs
+++ InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/PersonController.cs
@@ -1,5 +1,6 @@
 ﻿using InsuranceCorp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InsuranceCorp.MVC.Controllers
 {
@@ -17,13 +18,22 @@ namespace InsuranceCorp.MVC.Controllers
         public IActionResult Index()
         {
             // 1) získání dat / jejich zpracování
-            var top100 = _context.Persons
-                .OrderBy(person => person.Id)
+            var top100 = _context.Persons.Include(person => person.Constracts)
+                .OrderByDescending(person => person.Constracts.Count())
                 .Take(100).ToList();
 
+            ViewData["count"] = _context.Persons.Count();
             // 2) zobrazení dat uživateli
 
             return View(top100);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            // ziskat data
+            var person = _context.Persons.Find(id);
+
+            return View(person);
         }
     }
 }
