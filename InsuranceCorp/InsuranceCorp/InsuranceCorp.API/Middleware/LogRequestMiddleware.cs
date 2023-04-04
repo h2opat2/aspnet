f7798495aebe7e787cab4b60582693ea950c1b8d
h2opat2@gmail.com <h2opat2@gmail.com>
2023-04-04 16:37:18 +0200

    API + Middleware

---
+++ InsuranceCorp/InsuranceCorp/InsuranceCorp.API/Middleware/LogRequestMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using InsuranceCorp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace InsuranceCorp.API.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class LogRequestMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LogRequestMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext, InsCorpDbContext context)
+        {
+            // ziskat informace o requestu a odeslat do DB
+            var url = httpContext.Request.Path;
+            var ip = httpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+
+            // ulozeni do databaze
+            context.LogRequests.Add(new Model.RequestLog()
+            {
+                Date = DateTime.Now,
+                Url= url,
+                IP= ip
+            });
+
+            context.SaveChanges();
+
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class LogRequestMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLogRequestMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LogRequestMiddleware>();
+        }
+    }
+}
--- InsuranceCorp/InsuranceCorp/InsuranceCorp.API/Program.cs
+++ InsuranceCorp/InsuranceCorp/InsuranceCorp.API/Program.cs
@@ -1,3 +1,6 @@
+using InsuranceCorp.API.Middleware;
+using InsuranceCorp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<InsCorpDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +20,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseLogRequestMiddleware();
 
 // app.UseAuthorization();
 
